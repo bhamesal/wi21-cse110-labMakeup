ad2e21cf94a0f1d25b040ed7a4deb20196aead4f
bhamesal <59636267+bhamesal@users.noreply.github.com>
2021-02-05 15:00:51 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,26 @@
 // main.js
 
 // TODO
+
+var currentVolume = document.getElementById("volume-number");
+var soundVersion = document.getElementById("horn-sound");
+var soundImage = document.getElementById("sound-image");
+var soundTypeInput = document.getElementById("radio-air-horn")
+currentVolume.addEventListener("input", volChange);
+//soundTypeInput.addEventListener("click", typeChange);
+
+function volChange(currentVolume){
+  if(currentVolume.value > 66 && currentVolume.value <= 100){
+        horn-sound.value = currentVolume.value;
+    
+  }
+  else if(currentVolume.value > 33 && currentVolume.value <= 66){
+        horn-sound.value = currentVolume.value;
+  }
+  else if(currentVolume.value > 0 && currentVolume.value <= 33){
+        horn-sound.value = currentVolume.value;
+  }
+  else if(currentVolume.value == 0){
+        horn-sound.value = currentVolume.value;
+  }
+}
