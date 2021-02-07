95d4e7f5eb1b735fc31101b948a739d7cba62999
bhamesal <59636267+bhamesal@users.noreply.github.com>
2021-02-06 16:06:09 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,6 +2,7 @@
 
 // TODO
 
+var currentSlider = document.getElementById("volume-slider");
 var currentVolume = document.getElementById("volume-number");
 var soundVersion = document.getElementById("horn-sound");
 var soundImage = document.getElementById("sound-image");
@@ -10,17 +11,21 @@ currentVolume.addEventListener("input", volChange);
 //soundTypeInput.addEventListener("click", typeChange);
 
 function volChange(currentVolume){
+   
   if(currentVolume.value > 66 && currentVolume.value <= 100){
         horn-sound.value = currentVolume.value;
-    
+        currentSlider.value = currentVolume.value;
   }
   else if(currentVolume.value > 33 && currentVolume.value <= 66){
         horn-sound.value = currentVolume.value;
+        currentSlider.value = currentVolume.value;
   }
   else if(currentVolume.value > 0 && currentVolume.value <= 33){
         horn-sound.value = currentVolume.value;
+        currentSlider.value = currentVolume.value;
   }
   else if(currentVolume.value == 0){
         horn-sound.value = currentVolume.value;
+        currentSlider.value = currentVolume.value;
   }
 }
