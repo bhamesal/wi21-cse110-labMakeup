0e1e982c76c1c3e9067a2f9de01d68ef23e76e41
bhamesal <59636267+bhamesal@users.noreply.github.com>
2021-02-06 21:50:29 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,10 +2,32 @@
 
 // TODO
 
+document.getElementById("radio-air-horn").addEventListener("click", radAir);
+
+function radAir(){
+document.getElementById("sound-image").src = "./assets/media/images/air-horn.svg";
+document.getElementById("horn-sound").src = "./assets/media/audio/air-horn.mp3";
+}
+
+document.getElementById("radio-car-horn").addEventListener("click", radCar);
+
+function radCar(){
+document.getElementById("sound-image").src = "./assets/media/images/car.svg";
+document.getElementById("horn-sound").src = "./assets/media/audio/car-horn.mp3";
+}
+
+document.getElementById("radio-party-horn").addEventListener("click", radParty);
+
+function radParty(){
+document.getElementById("sound-image").src = "./assets/media/images/party-horn.svg";
+document.getElementById("horn-sound").src = "./assets/media/audio/party-horn.mp3";
+}
+   
+/*
 document.getElementById("honk-btn").addEventListener("submit", function(event){
    event.preventDefault()
 });
-/*
+
 var currentSlider = document.getElementById("volume-slider");
 var currentVolume = document.getElementById("volume-number");
 var soundVersion = document.getElementById("horn-sound");
